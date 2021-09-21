546b6d9d5a6d96cb98ba234e88760fecd1157800
Marek Bartík <bartimar6@gmail.com>
2021-09-21 11:59:15 +0200

    feat: ingress, dns, tls & cheatsheet

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,3 +1,4 @@
-region      = "eu-west-1"
-environment = "dev"
-k8s_version = "1.21"
+region          = "eu-west-1"
+environment     = "dev"
+k8s_version     = "1.21"
+dns_zone_suffix = "eks-demo.pipetail.cloud"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,9 @@ variable "environment" {
 variable "k8s_version" {
   type = string
 }
+
+variable "dns_zone_suffix" {
+  type        = string
+  description = "DNS zone suffix to use with public DNS domains"
+  default     = "acme.org"
+}
