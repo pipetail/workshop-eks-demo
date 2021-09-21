7e0d8d9c4a26f8e9ccb7002c314d978d9571d04f
Stepan Vrany <stepan@vrany.dev>
2021-09-21 15:04:47 +0200

    feat: add scenario with restricted perms.

---
+++ 05_node_user/README.md
@@ -0,0 +1,6 @@
+## build
+
+```bash
+docker build -t 859133351452.dkr.ecr.eu-west-1.amazonaws.com/backend:v0.0.2 --platform linux/amd64 .
+```
+
---
+++ 05_node_user/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+var winston = require('winston')
+expressWinston = require('express-winston')
+
+const app = express()
+const port = 3000
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  format: winston.format.combine(
+    winston.format.json()
+  ),
+  meta: true,
+  expressFormat: true,
+  colorize: false,
+  ignoreRoute: function (req, res) { return false; }
+}));
+
+app.get('/_probe/ready', (req, res) => {
+  res.send("ready")
+})
+
+app.get('/_probe/alive', (req, res) => {
+  res.send("alive")
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World (2nd)!')
+})
+
+const server = app.listen(port)
+
